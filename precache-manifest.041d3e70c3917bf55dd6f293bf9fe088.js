519915ba9c04a960db7d41faae6666ecf10a4a73
Brenda <apples@BrendaJ.local>
2020-04-16 20:28:41 +0300

    Updates

---
+++ precache-manifest.041d3e70c3917bf55dd6f293bf9fe088.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c068a2950656177478bd739b366dbc93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb60cfe8a95ec5ed89fc",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "ff2ec1d61738789d2cfe",
+    "url": "/static/js/2.59e3010b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.59e3010b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb60cfe8a95ec5ed89fc",
+    "url": "/static/js/main.ee37c06c.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "894c8e1a62da4d884538b4e83a9397e2",
+    "url": "/static/media/artfunder.894c8e1a.png"
+  },
+  {
+    "revision": "6c1c7ffc4f8f9172ab8baca4bb6ffba2",
+    "url": "/static/media/dadjokes.6c1c7ffc.png"
+  },
+  {
+    "revision": "28d40076058ddc57921ee6b23b26055f",
+    "url": "/static/media/foodiepass.28d40076.png"
+  },
+  {
+    "revision": "470c7751d405ce87de4c2e582b278157",
+    "url": "/static/media/hero.470c7751.jpg"
+  },
+  {
+    "revision": "bcd4a9cabd8d82d5093b8ab6bed6f188",
+    "url": "/static/media/me.bcd4a9ca.jpg"
+  },
+  {
+    "revision": "553b392dc1976c35d42d1b3a5788e03e",
+    "url": "/static/media/partyplanner.553b392d.png"
+  }
+]);
